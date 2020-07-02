a02af27e7ae2aaf3fc9914bae9e3774d00bfb219
Kimberlyn Stoddard <kimberlynrstoddard@gmail.com>
2020-07-02 15:17:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -43,10 +43,10 @@ def monopoly_with_fourth_tier
         :mortgage_value => 100
       },
       :pennsylvania_railraod => {
-        :mortgage_value: 200
+        :mortgage_value => 200
       },
       :b_and_o_railroad => {
-        :mortgage_value: 400
+        :mortgage_value => 400
       },
       :shortline_railroad => {
         :mortgage_value => 800
